242a42ce387b26a5d0d41f3d379db41905db320d
aichaettabet <aicha70tabite@gmail.com>
2024-04-02 23:09:04 +0000

    add api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\PersonalInformationController;
+use App\Http\Controllers\EducationController;
+use App\Http\Controllers\ExperienceController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->apiResource('personal-information', PersonalInformationController::class);
+
+Route::middleware('auth:sanctum')->apiResource('education', EducationController::class);
+
+Route::middleware('auth:sanctum')->apiResource('experience', ExperienceController::class);
