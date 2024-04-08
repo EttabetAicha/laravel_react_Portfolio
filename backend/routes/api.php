d53795a9d73dfa6f18475b9ad44ef4f45c4d69b3
aichaettabet <aicha70tabite@gmail.com>
2024-04-08 11:13:44 +0000

    edit route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::resource('users',UserController::class);
 // Route::resource('personal-information',PersonalInformationController::class);
