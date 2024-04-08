f766699ddbbb913c41122711d858b1828975e473
aichaettabet <aicha70tabite@gmail.com>
2024-04-08 11:14:15 +0000

    add auth

--- frontend/src/sections/login/login-view.jsx
+++ frontend/src/sections/login/login-view.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 import Box from '@mui/material/Box';
@@ -20,28 +21,51 @@ import { bgGradient } from 'src/theme/css';
 import Logo from 'src/components/logo';
 import Iconify from 'src/components/iconify';
 
-// ----------------------------------------------------------------------
 
 export default function LoginView() {
   const theme = useTheme();
-
   const router = useRouter();
-
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error,setError] = useState('');
 
-  const handleClick = () => {
-    router.push('/dashboard');
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post('http://localhost:8000/api/login', { email, password }); 
+      console.log(response);
+      const { token } = response.data;
+      localStorage.setItem('token', token);
+      router.push('/app');
+    } catch (e) {
+      if (e.response && e.response.data && e.response.data.error) {
+        setError(error.response.data.error);
+      } else {
+        setError('An error occurred during login.');
+      }
+    } finally {
+      setLoading(false);
+    }
   };
+  
 
   const renderForm = (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
-
+        <TextField
+          name="email"
+          label="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <TextField
           name="password"
           label="Password"
           type={showPassword ? 'text' : 'password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -53,23 +77,23 @@ export default function LoginView() {
           }}
         />
       </Stack>
-
       <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{ my: 3 }}>
         <Link variant="subtitle2" underline="hover">
           Forgot password?
         </Link>
       </Stack>
-
       <LoadingButton
         fullWidth
         size="large"
         type="submit"
         variant="contained"
         color="inherit"
-        onClick={handleClick}
+        loading={loading}
+        onClick={handleLogin}
       >
         Login
       </LoadingButton>
+      {error && <Typography color="error">{error}</Typography>}
     </>
   );
 
@@ -90,7 +114,6 @@ export default function LoginView() {
           left: { xs: 16, md: 24 },
         }}
       />
-
       <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
         <Card
           sx={{
@@ -100,14 +123,12 @@ export default function LoginView() {
           }}
         >
           <Typography variant="h4">Sign in to Minimal</Typography>
-
           <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
             Don’t have an account?
             <Link variant="subtitle2" sx={{ ml: 0.5 }}>
               Get started
             </Link>
           </Typography>
-
           <Stack direction="row" spacing={2}>
             <Button
               fullWidth
@@ -118,7 +139,6 @@ export default function LoginView() {
             >
               <Iconify icon="eva:google-fill" color="#DF3E30" />
             </Button>
-
             <Button
               fullWidth
               size="large"
@@ -128,7 +148,6 @@ export default function LoginView() {
             >
               <Iconify icon="eva:facebook-fill" color="#1877F2" />
             </Button>
-
             <Button
               fullWidth
               size="large"
@@ -139,13 +158,11 @@ export default function LoginView() {
               <Iconify icon="eva:twitter-fill" color="#1C9CEA" />
             </Button>
           </Stack>
-
           <Divider sx={{ my: 3 }}>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
               OR
             </Typography>
           </Divider>
-
           {renderForm}
         </Card>
       </Stack>
