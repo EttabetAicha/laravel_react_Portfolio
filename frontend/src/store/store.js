1e2c2ed87cab00d86b89ba41e063cea4348af7a9
aichaettabet <aicha70tabite@gmail.com>
2024-04-16 11:02:40 +0100

    update store redux

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,15 @@
-import thunk from 'redux-thunk'; 
 import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const asyncMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'function') {
+        return action(store.dispatch, store.getState);
+    }
+
+    return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(asyncMiddleware));
 
 export default store;
